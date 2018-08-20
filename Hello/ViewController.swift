55a181ce17165ee697f5d7d9376b2d4b193e5abd
thanatat <thanatat.b@kkumail.com>
2018-08-20 16:06:06 +0700

    First Commit

--- Hello/ViewController.swift
+++ Hello/ViewController.swift
@@ -10,9 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var isLightTheme = true
+    
+    @IBOutlet weak var lblHello: UILabel!
+    @IBOutlet weak var txtName: UITextField!
+    
+    @IBAction func SayHiPressed(_ sender: Any) {
+        self.lblHello.text = "Hello \(self.txtName.text!)"
+    }
+    
+    @IBAction func changeTheme(_ sender: Any) {
+        if isLightTheme == true {
+            isLightTheme = false
+            for eachView in view.subviews {
+                view.backgroundColor = UIColor.black
+                if let lable = eachView as? UILabel {
+                    lable.textColor = UIColor.white
+                }
+            }
+        } else {
+            isLightTheme = true
+            for eachView in view.subviews {
+                view.backgroundColor = UIColor.white
+                if let lable = eachView as? UILabel {
+                    lable.textColor = UIColor.black
+                }
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.lblHello.text = "Hi"
     }
 
     override func didReceiveMemoryWarning() {
