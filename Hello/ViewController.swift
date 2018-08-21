e392686080b4948a733d0a236ac3f2df923f5d6e
thanatat <thanatat.b@kkumail.com>
2018-08-21 09:36:24 +0700

    Add new Logo

--- Hello/ViewController.swift
+++ Hello/ViewController.swift
@@ -42,6 +42,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.lblHello.text = "Hi"
+        print("kookoko")
     }
 
     override func didReceiveMemoryWarning() {
